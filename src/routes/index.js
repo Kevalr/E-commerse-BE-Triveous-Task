68ab695623083703fc3ab8af950e23e43a15fe5b
Keval <kevalradadiya9825@gmail.com>
2024-04-05 14:46:29 +0530

    added device check support

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,17 @@ const authenticateUser = require("../middleware/auth");
 
 const router = Router();
 
+router.get("/checkDevice", (req, res) => {
+    const userAgent = require("express-useragent");
+    const DeviceDetector = require("node-device-detector");
+
+    const detector = new DeviceDetector();
+
+    const agent = req.headers['user-agent'];
+
+    res.json({"device detector : ": detector.detect(agent), "useragent : ": userAgent.parse(agent)});
+
+});
 router.use("/auth", authRoutes);
 router.use("/products", productRoutes);
 router.use("/cart", authenticateUser, cartRoutes);
